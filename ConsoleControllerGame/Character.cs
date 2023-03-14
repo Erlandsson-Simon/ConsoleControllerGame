4a6aabc4fa716cddf531d1e4adbb03df420f1d9a
Erlandsson-Simon <erlandsson.simon@elev.ga.ntig.se>
2023-03-14 13:31:04 +0100

    ConsoleGAME BEGIN

---
+++ ConsoleControllerGame/Character.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace ConsoleControllerGame;
+
+public class Character
+{
+    public string Name { get; set; }
+    public int Hp { get; set; }
+}
---
+++ ConsoleControllerGame/Program.cs
@@ -0,0 +1,21 @@
+﻿global using System;
+global using System.Numerics;
+global using Raylib_cs;
+global using R = Raylib_cs.Raylib;
+
+R.InitWindow(1600, 800, "JumpMaster");
+R.SetTargetFPS(60);
+
+
+
+while (!R.WindowShouldClose())
+{
+    R.BeginDrawing();
+
+    R.ClearBackground(Color.WHITE);
+
+    R.EndMode2D();
+    R.EndDrawing();
+}
+
+// Console.WriteLine(Raylib.GetGamepadAxisMovement(0, GamepadAxis.GAMEPAD_AXIS_LEFT_X))
